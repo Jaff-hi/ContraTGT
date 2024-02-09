8dd088e5f11ba9fdc4666b3d445898c6c8bb8f21
Jaff-hi <58620048+Jaff-hi@users.noreply.github.com>
2024-02-09 10:08:59 +0800

    Update sampling.py

--- sampling.py
+++ sampling.py
@@ -7,12 +7,12 @@ import numpy as np
 def get_adj_list(edge):
     adj_list = defaultdict(list)
     for i,edg in enumerate(edge['idx']):
-        st = int(edg[0])#边的起点
-        en = int(edg[1])#边的终点
-        ts = int(edg[2])#边的时间戳
-        idx = int(edg[3])#边的索引
-        adj_list[st].append((en,ts,idx))#在st节点的邻接节点集合中添加en节点
-        adj_list[en].append((st,ts,idx)) #若为有向图，这一步可以去掉
+        st = int(edg[0])
+        en = int(edg[1])
+        ts = int(edg[2])
+        idx = int(edg[3])
+        adj_list[st].append((en,ts,idx))
+        adj_list[en].append((st,ts,idx)) 
     return adj_list
 
 def init_offset(adj_list):
@@ -21,12 +21,12 @@ def init_offset(adj_list):
     idx_l = []
     offset_l = [0]
     for i in range(len(adj_list)):
-        curr = adj_list[i]  # curr记录为当前i节点
-        curr = sorted(curr, key=lambda x: x[2])  # 按照时间戳排序
-        node_l.extend([x[0] for x in curr])  # 将相应的邻居节点append到node_l中
-        ts_l.extend([x[1] for x in curr])  # 将邻居节点对应的时间戳保存
-        idx_l.extend([x[2] for x in curr])  # 保存idx
-        offset_l.append(len(node_l))  # 记录每个节点的邻节点个数
+        curr = adj_list[i]  
+        curr = sorted(curr, key=lambda x: x[2])  
+        node_l.extend([x[0] for x in curr])  
+        ts_l.extend([x[1] for x in curr])  
+        idx_l.extend([x[2] for x in curr])  
+        offset_l.append(len(node_l))  
 
     node_l = np.array(node_l)
     ts_l = np.array(ts_l)
@@ -36,16 +36,16 @@ def init_offset(adj_list):
     return node_l, ts_l, idx_l, offset_l
 
 def find_before(node_l, ts_l, idx_l, offset_l, node, ts):
-    ngh_node_l = node_l[offset_l[node]:offset_l[node + 1]]  # 对应u节点的邻居
-    ngh_ts_l = ts_l[offset_l[node]:offset_l[node + 1]]  # 对应u节点的时间戳
-    ngh_idx_l = idx_l[offset_l[node]:offset_l[node + 1]]  # 对应u节点交互的顺序idx
+    ngh_node_l = node_l[offset_l[node]:offset_l[node + 1]]  
+    ngh_ts_l = ts_l[offset_l[node]:offset_l[node + 1]]  
+    ngh_idx_l = idx_l[offset_l[node]:offset_l[node + 1]]  
 
     if len(ngh_node_l) == 0 or len(ngh_ts_l) == 0:
         return ngh_node_l, ngh_ts_l, ngh_idx_l
 
     left = 0
     right = len(ngh_node_l) - 1
-    while left + 1 < right:  # 二分法找到对应的时间戳
+    while left + 1 < right:  
         mid = (left + right) // 2
         curr_t = ngh_ts_l[mid]
         if curr_t < ts:
@@ -53,7 +53,7 @@ def find_before(node_l, ts_l, idx_l, offset_l, node, ts):
         else:
             right = mid
 
-    if ngh_ts_l[right] < ts:  # 返回对应时间戳之前的所有交互
+    if ngh_ts_l[right] < ts:  
         return ngh_node_l[:right], ngh_idx_l[:right], ngh_ts_l[:right]
     else:
         return ngh_node_l[:left], ngh_idx_l[:left], ngh_ts_l[:left]
@@ -61,13 +61,13 @@ def find_before(node_l, ts_l, idx_l, offset_l, node, ts):
 
 def get_neighbor_list(node_l, ts_l, idx_l, offset_l, node, timestamp, num_sample):
     #     print(timestamp)
-    ngh_node = np.zeros((len(node), num_sample)).astype(np.int32)  # 建一个【len，num_sample】的0矩阵保存序列
-    ngh_ts = np.zeros((len(node), num_sample)).astype(np.int32)  # 保存时间戳
-    ngh_idx = np.zeros((len(node), num_sample)).astype(np.int32)  # 保存idx
-    ngh_mask = np.zeros((len(node), num_sample)).astype(np.int32)  # 保存mask矩阵
+    ngh_node = np.zeros((len(node), num_sample)).astype(np.int32)  
+    ngh_ts = np.zeros((len(node), num_sample)).astype(np.int32)  
+    ngh_idx = np.zeros((len(node), num_sample)).astype(np.int32)  
+    ngh_mask = np.zeros((len(node), num_sample)).astype(np.int32)  
     num_sample = num_sample - 1
     for i, edg in enumerate(zip(node, timestamp)):
-        node, idx, ts = find_before(node_l, ts_l, idx_l, offset_l, edg[0], edg[1])  # 根据给定的源节点和时间戳，找时间戳之前发生的邻居节点
+        node, idx, ts = find_before(node_l, ts_l, idx_l, offset_l, edg[0], edg[1])  
         node = node[::-1]
         idx = idx[::-1]
         ts = ts[::-1]
@@ -75,7 +75,7 @@ def get_neighbor_list(node_l, ts_l, idx_l, offset_l, node, timestamp, num_sample
         #         print('*********************')
         #         print(int(edg[1])-ts)
         if len(node) > 0:
-            if len(node) > num_sample:  # 如果个数大于采样需求的数量，只保留最近的num_sample条数据
+            if len(node) > num_sample:  
                 node = node[:num_sample]
                 idx = idx[:num_sample]
                 ts = ts[:num_sample]
@@ -92,34 +92,34 @@ def get_neighbor_list(node_l, ts_l, idx_l, offset_l, node, timestamp, num_sample
 def get_interaction_list(edge):
     idx_list = defaultdict(list)
     for i,edg in enumerate(edge['idx']):
-        st = int(edg[0])#边的起点
-        en = int(edg[1])#边的终点
-        idx = int(edg[3])#边的索引
-        idx_list[st].append(idx)#在st节点的邻接节点集合中添加en节点
-        idx_list[en].append(idx) #若为有向图，这一步可以去掉
+        st = int(edg[0])
+        en = int(edg[1])
+        idx = int(edg[3])
+        idx_list[st].append(idx)
+        idx_list[en].append(idx) 
     return idx_list
 
 
 def get_unique_node_sequence(b_edge, f_edge, k, idx_list, flag):
-    num_nodes = len(b_edge['idx'])  # 确定节点个数
-    node_sequence = np.zeros((num_nodes, k)).astype(np.int32)  # 用于保存节点序列
-    node_timestamp = np.zeros((num_nodes, k)).astype(np.int32)  # 用于保存时间差
-    node_seq_mask = np.zeros((num_nodes, k)).astype(np.int32)  # 用于保存mask矩阵
+    num_nodes = len(b_edge['idx'])  
+    node_sequence = np.zeros((num_nodes, k)).astype(np.int32)  
+    node_timestamp = np.zeros((num_nodes, k)).astype(np.int32) 
+    node_seq_mask = np.zeros((num_nodes, k)).astype(np.int32)  
     row = int(k / 2)  # 5
-    for i, edg in enumerate(b_edge['idx']):  # 遍历每一行数据
+    for i, edg in enumerate(b_edge['idx']):  
         ts = int(edg[2])
         idx = int(edg[3])
-        # node序列
-        if flag:  # 目标节点
+        # node sequence
+        if flag:  
             from_node = int(edg[0])
-        else:  # 目的节点
+        else:  
             from_node = int(edg[1])
         if idx in idx_list[from_node]:
             index = idx_list[from_node].index(idx)
         else:
             left = 0
             right = len(idx_list[from_node]) - 1
-            while left + 1 < right:  # 二分法找到对应的时间戳
+            while left + 1 < right:  
                 mid = (left + right) // 2
                 curr_idx = idx_list[from_node][mid]
                 if curr_idx < idx:
@@ -130,34 +130,34 @@ def get_unique_node_sequence(b_edge, f_edge, k, idx_list, flag):
         if index > 0:
             last_event = idx_list[from_node][index - 1]
             if last_event < row:
-                node = np.zeros(2 * last_event).astype(np.int32)  # 创建一个0矩阵
+                node = np.zeros(2 * last_event).astype(np.int32)  
                 time = np.zeros(2 * last_event).astype(np.int32)
                 for j, ed in enumerate(f_edge['idx'][0:last_event]):
                     node[2 * j] = ed[1]
                     node[2 * j + 1] = ed[0]
                     time[2 * j] = ts - int(ed[2])
                     time[2 * j + 1] = ts - int(ed[2])
-                node = node[::-1]  # 排列倒置
+                node = node[::-1]  
                 time = time[::-1]
                 # print(node)
                 node_timestamp[i, 1:2 * last_event + 1] = time
-                node_sequence[i, 1:2 * last_event + 1] = node  # 存入节点序列中
-                node_seq_mask[i, 1:2 * last_event + 1] = 1  # 保存mask矩阵
+                node_sequence[i, 1:2 * last_event + 1] = node  
+                node_seq_mask[i, 1:2 * last_event + 1] = 1  
             else:
-                node = np.zeros(2 * row).astype(np.int32)  # 创建一个0矩阵
+                node = np.zeros(2 * row).astype(np.int32)  
                 time = np.zeros(2 * row).astype(np.int32)
                 for j, ed in enumerate(f_edge['idx'][last_event - row:last_event]):
                     node[2 * j] = ed[1]
                     node[2 * j + 1] = ed[0]
                     time[2 * j] = ts - int(ed[2])
                     time[2 * j + 1] = ts - int(ed[2])
-                node = node[::-1]  # 排列倒置
+                node = node[::-1]  
                 time = time[::-1]
                 node_timestamp[i, 1:] = time
-                node_sequence[i, 1:] = node  # 存入节点序列中
-                node_seq_mask[i, 1:] = 1  # 保存mask矩阵
+                node_sequence[i, 1:] = node  
+                node_seq_mask[i, 1:] = 1  
         node_timestamp[i, 0] = 0
-        node_sequence[i, 0] = from_node  # 存入节点序列中
-        node_seq_mask[i, 0] = 1  # 保存mask矩阵
+        node_sequence[i, 0] = from_node  
+        node_seq_mask[i, 0] = 1  
 
     return node_sequence, node_seq_mask, node_timestamp
